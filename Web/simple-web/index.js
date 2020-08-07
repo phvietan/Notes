010140f6e48b07a57c6d17e3432b6b037df1305f
DokterStrength <phvietan@gmail.com>
2020-08-07 11:01:34 +0700

    update

--- Web/simple-web/index.js
+++ Web/simple-web/index.js
@@ -1,40 +1,50 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+
 const express = require('express');
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 app.use(express.static('public'));
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/a.google.com', function (req, res) {
-  res.send('console.log("a");');
-  console.log('injected successfully');
+function logJsonObject(obj) {
+    const arrKeyVal = Object.entries(obj);
+    if (arrKeyVal.length == 0) {
+        console.log("{}");
+        return;
+    }
+    arrKeyVal.forEach(val => {
+        const key = val[0];
+        const value = val[1];
+        console.log(`"${key}": "${value}"`);
+    });
+}
+
+app.use((req, _, next) => {
+    console.log(`From ip ${req.ip}`);
+    console.log(`Receive ${req.method} request at: ${req.url}`);
+
+    console.log("======================");
+    console.log("======================");
+    console.log("Receive headers");
+    logJsonObject(req.headers);
+
+    console.log("======================");
+    console.log("======================");
+    console.log("Receive body");
+    logJsonObject(req.body);
+
+    console.log("------------------------------------------------------------------------------------------------")
+    console.log("------------------------------------------------------------------------------------------------")
+    next();
 });
 
-app.get('/test', function (req, res) {
-  res.sendFile('/root/Documents/Make-Us-Great-Again/AnPham/Notes/Web/simple-web/test.html');
-});
-
-app.get('/*', function (req, res) {
-    res.send('Hello World');
-    console.log('Receive GET request at:', req.url);
-    console.log('---------------');
-});
-
-app.post('/*', function (req, res) {
-    res.send('Hello World');
-    console.log('Receive POST request at:', req.url);
-    console.log('Data received:');
-    console.log(JSON.stringify(req.body));
-    console.log('---------------');
-});
-
-const server = app.listen(80, function () {
+const server = app.listen(PORT, function () {
    const host = server.address().address;
    const port = server.address().port;
-
    console.log("App listening at http://%s:%s", host, port);
 })
